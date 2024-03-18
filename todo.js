b44e48043fcfd153e3e6f8eab6f0aed7c29cf6c5
Kaden Stone <kadencsschool@gmail.com>
2024-03-18 16:47:18 -0600

    update: working on more of the javascript:

---
+++ todo.js
@@ -0,0 +1,26 @@
+const inputArea = document.getElementById("input-area");
+const listContain = document.getElementById("list-contain");
+
+function addTask(){
+    if(inputArea.value === ''){
+        alert("You'll wanna write something here!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputArea.value;
+        listContain.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputArea.value = "";
+}
+
+listContain.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("check");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
